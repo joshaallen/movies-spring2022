a0f47fc10e6dbf73c336d4da947e1687c2045300
Joshua Allen <jaalle10@dmacc.edu>
2022-04-04 20:14:48 -0500

    Added my name to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {movies} from './movies';
 const App = () => {
     return (
         <div>
+            <p>Josh Allen</p>
             <pre>{JSON.stringify(movies, null, 4)}</pre>
         </div>
     );
