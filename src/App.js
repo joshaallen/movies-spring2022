c27d2cf57e42bb8cc82a9dd4688a3d842b3bdab0
joshaallen <nighthawk@joshuaallen.info>
2022-04-22 20:24:44 -0500

    arrow function vs old way of writing function in map function

--- src/App.js
+++ src/App.js
@@ -7,35 +7,26 @@ const App = () => {
 
     return (
         <div>
-            {/*Using JSX fragments to handle adjacent JSX elements for the return array of the map function*/}
+            {/*Comparing arrow function with old function*/}
+           {
+            movies.map((movie) =>
+            <>
+                <p>{movie.title}</p>
+                <img src={movie.posterUrl}/>
+            </>
+            )
+           }
+            {/*Implementing 'old way of writng function' in the map function*/}
             {
-                movies.map((movie) => 
-                <>
-                    <p>{movie.title}</p>
-                    <img src={movie.posterUrl}/>
-                    <p>{movie.rating}</p>
-                    <p>{movie.director}</p>
-                </>
-                   
+            movies.map(function(movie) {
+                return (
+                    <>
+                        <p>{movie.title}</p>
+                        <img src={movie.posterUrl}/>
+                    </>
                 )
-            }
-             {/*Other than using JSX elements we can use an HTML container (In this case div) to wrap the JSX elements to avoid error*/}
-            {
-                movies.map((movie) =>
-                <div>
-                     <p>{movie.title}</p>
-                    <img src={movie.posterUrl}/>
-                    <p>{movie.rating}</p>
-                    <p>{movie.director}</p>
-                </div>
-                   
-                )
-            }
-             {/*Looping through movie array and returning posters in an image tag*/}
-             {
-               movies.map((movie) => <img src={movie.posterUrl}/>)
-             }
-          
+            })
+           }
         </div>
     );
 }
