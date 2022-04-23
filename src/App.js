2463e8cb67e337d13c13bfc4b050385424a6bcbd
joshaallen <nighthawk@joshuaallen.info>
2022-04-23 15:44:15 -0500

    added component MoviePreview and prop movieToPreview

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import MoviePreview from './MoviePreview';
 import {movies} from './movies';
-import {useContext, useReducer, useState} from "react";
+
 
 const App = () => {
 
     return (
         <div>
-            {/*Added alt atribute to satisfy React warnings*/}
            {
             movies.map((movie) =>
             <>
-                <p>{movie.title}</p>
-                <img src={movie.posterUrl} alt={movie.title}/>
+               <MoviePreview
+                movieToPreview={movie}
+               />
             </>
             )
            }
