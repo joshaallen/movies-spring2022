60352c50ae1c486d6b255d20c97e9c72e14ce6fd
joshaallen <nighthawk@joshuaallen.info>
2022-04-23 16:31:32 -0500

    currentMovie in state, and some notes on destructuring an array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 import MoviePreview from './MoviePreview';
 import {movies} from './movies';
+import {useState} from 'react';
 
 
 const App = () => {
+    //this line is the same as the next two
+    //you can do javascript here
+    //example using console.log
+    console.log("Hello");
+    /*
+      function useState(startingState)  {
+          //some logic goes here
+
+          return [variable, setVariable];
+      } 
+    */
+   const [currentMovie, setCurrentMovie] = useState(null);
+      
+    //const currentMovie = useState(null)[0];
+    //const setCurrentMovie = useState(null)[1];
+
 
     return (
         <div className="movie-container">
