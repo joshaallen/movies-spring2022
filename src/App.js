aa172f690b77911dd34bf334e5b622bb44dd1fe3
joshaallen <nighthawk@joshuaallen.info>
2022-04-22 19:44:14 -0500

    previous commit w.o. extra console.log implementations

--- src/App.js
+++ src/App.js
@@ -5,22 +5,6 @@ import {useContext, useReducer, useState} from "react";
 
 const App = () => {
 
-//you can do js here
-console.log('hello');
-
-/*
-function useState(startingState) {
-    //some login goes here
-    return value [variable, setVariableFunction];
-}
-*/
-
-const [currentMovie, setCurrentMovie] = useState(null);
-//this line is the same as the next two
-//const currentMovie = useState(null)[0]
-//const setCurrentMovie = useState(null)[1]
-
-console.log(currentMovie);
     return (
         <div>
             {/*The following is looping over movie array and returning movie titles then looping over movie array and returning movie posters in the img tag*/}
