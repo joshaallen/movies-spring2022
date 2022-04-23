6c618bccdeddf9eeb9f8ddf6fb4c6bc253f015ba
joshaallen <nighthawk@joshuaallen.info>
2022-04-22 19:35:20 -0500

    looping over movie array output title and img seperately

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import './App.css';
+import MoviePreview from './MoviePreview';
 import {movies} from './movies';
+import {useContext, useReducer, useState} from "react";
 
 const App = () => {
+
+//you can do js here
+console.log('hello');
+
+/*
+function useState(startingState) {
+    //some login goes here
+    return value [variable, setVariableFunction];
+}
+*/
+
+const [currentMovie, setCurrentMovie] = useState(null);
+//this line is the same as the next two
+//const currentMovie = useState(null)[0]
+//const setCurrentMovie = useState(null)[1]
+
+console.log(currentMovie);
     return (
         <div>
-            {/*The following .map is equivalent to the 10 <p> elements below*/}
+            {/*The following is looping over movie array and returning movie titles then looping over movie array and returning movie posters in the img tag*/}
             {
                 movies.map((movie) => <p>{movie.title}</p>)
             }
-            {
-                movies.map((movie) => <p>{movie.title}</p>)
-            }
-
-            <p>{movies[0].title}</p>
-            <p>{movies[1].title}</p>
-            <p>{movies[2].title}</p>
-            <p>{movies[3].title}</p>
-            <p>{movies[4].title}</p>
+            
+             {
+               movies.map((movie) => <img src={movie.posterUrl}/>)
+             }
+          
         </div>
     );
 }
