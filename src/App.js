cbe40c7e96a48e96a169090b54489157e6345889
joshaallen <nighthawk@joshuaallen.info>
2022-04-23 15:27:01 -0500

    created a prop named movieToPreview that is destructured in our MoviePreview component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import './App.css';
-import MoviePreview from './MoviePreview';
 import {movies} from './movies';
-import {useContext, useReducer, useState} from "react";
+import MoviePreview from "./MoviePreview";
 
 const App = () => {
 
     return (
         <div>
-            {/*Added alt atribute to satisfy React warnings*/}
            {
             movies.map((movie) =>
             <>
-                <p>{movie.title}</p>
-                <img src={movie.posterUrl} alt={movie.title}/>
+                <MoviePreview
+                    movieToPreview={movie}
+                />
             </>
             )
            }
