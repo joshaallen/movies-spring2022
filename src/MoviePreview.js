c2cfa1352f3c9a23a5c7f8d4fbb886b0e043d01d
joshaallen <nighthawk@joshuaallen.info>
2022-04-24 20:02:54 -0500

    added clickCount function in an inline style(HTML Attribute)

--- src/MoviePreview.js
+++ src/MoviePreview.js
@@ -1,23 +1,22 @@
-const MoviePreview = (props) => {
-//props is an object
-//const props = {
-//    moviesToPrieview: movie
-//  setCurrentMovie: a function
-//}
+import { useState } from "react";
 
-//this is using destructing to access the moviesToPreview props
+
+const MoviePreview = (props) => {
+
 const {movieToPreview, setCurrentMovie} = props;
-//below this is the long way to destructure the above line
-//const moviesToPreview = props.moviesToPreview;
-//w.o. the arrow function () => bc we are passing a parameter 
-// the app would reload for each movie arrow functions behaves
-// like a anonymous function that wraps our named function so //it doesn't get called on run time
-//reactEvents are written in camelCase
+
+const [clickCount, setClickCount] = useState(0);
+
     return (
         <div>
             <p>{movieToPreview.title}</p>
+            <p>{clickCount}</p>
             <img src={movieToPreview.posterUrl} alt={movieToPreview.title} 
-            onClick={() => setCurrentMovie(movieToPreview)}
+            onClick={() => {
+                
+                setCurrentMovie(movieToPreview);
+                setClickCount(clickCount + 1);
+            }}
             />
         </div>
         
